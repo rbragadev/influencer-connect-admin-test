ce266e2d70f2c6fee9da3a09040fa8223ec1bcc9
Raphael Braga <raffael.info@gmail.com>
2023-01-18 16:32:21 -0300

    feat: make login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 
+import { LoginComponent } from './views/login/login.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     CardModule,
+    InputTextModule,
     ButtonModule,
   ],
   providers: [],
